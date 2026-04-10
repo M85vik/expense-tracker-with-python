800e026957020faba732bc167ded62e7504e27eb
Vikas Sharma <vikas@Vikas.local>
2026-04-10 13:16:21 +0530

    summary route added

--- app/features/expenses/router/expense_router.py
+++ app/features/expenses/router/expense_router.py
@@ -3,7 +3,7 @@ from typing import Optional
 from datetime import date
 from app.features.expenses.service.expense_service import ExpenseService
 
-from app.features.expenses.schemas.expense_schema import ExpenseCreate , ExpenseResponse
+from app.features.expenses.schemas.expense_schema import ExpenseCreate, ExpenseResponse
 from typing import List
 router = APIRouter()
 service = ExpenseService()
@@ -21,3 +21,18 @@ async def get_expenses(
     end_date: Optional[date] = None
 ):
     return await service.get_expenses(category, start_date, end_date)
+
+@router.get("/expenses/summary")
+async def get_summary(month: int, year: int):
+    return await service.get_summary(month, year)
+
+@router.get("/expenses/{id}", response_model=ExpenseResponse)
+async def get_expense_by_id(id: int):
+    return await service.get_expense_by_id(id)
+
+
+
+
+@router.delete("/expenses/{id}")
+async def delete_expense(id: int):
+    return await service.delete_expense(id)
